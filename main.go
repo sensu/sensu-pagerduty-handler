09f82fc5afde0a1f4f80ea2da7968b11491f865f
sayakghosh <sayak.ghosh@sixt.com>
2019-05-25 21:47:34 +0530

    Integration with sensu-plugins-go-library

--- main.go
+++ main.go
@@ -1,90 +1,51 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/PagerDuty/go-pagerduty"
-	"github.com/sensu/sensu-go/types"
-	"github.com/spf13/cobra"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-plugins-go-library/sensu"
 )
 
-var (
+type HandlerConfig struct {
+	sensu.PluginConfig
 	authToken string
-	stdin     *os.File
+}
+
+var (
+	config = HandlerConfig{
+		PluginConfig: sensu.PluginConfig{
+			Name:  "sensu-pagerduty-handler",
+			Short: "The Sensu Go PagerDuty handler for incident management",
+		},
+	}
+
+	pagerDutyConfigOptions = []*sensu.PluginConfigOption{
+		{
+			Path:      "token",
+			Env:       "PAGERDUTY_TOKEN",
+			Argument:  "token",
+			Shorthand: "t",
+			Usage:     "The PagerDuty V2 API authentication token, use default from PAGERDUTY_TOKEN env var",
+			Value:     &config.authToken,
+		},
+	}
 )
 
 func main() {
-	rootCmd := configureRootCommand()
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
+	goHandler := sensu.NewGoHandler(&config.PluginConfig, pagerDutyConfigOptions, checkArgs, manageIncident)
+	goHandler.Execute()
 }
 
-func configureRootCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "sensu-pagerduty-handler",
-		Short: "The Sensu Go PagerDuty handler for incident management",
-		RunE:  run,
-	}
-
-	/*
-	   Security Sensitive flags
-	     - default to using envvar value
-	     - do not mark as required
-	     - manually test for empty value
-	*/
-	cmd.Flags().StringVarP(&authToken,
-		"token",
-		"t",
-		os.Getenv("PAGERDUTY_TOKEN"),
-		"The PagerDuty V2 API authentication token, use default from PAGERDUTY_TOKEN env var")
-
-	return cmd
-}
-
-func run(cmd *cobra.Command, args []string) error {
-	if len(args) != 0 {
-		_ = cmd.Help()
-		return fmt.Errorf("invalid argument(s) received")
-	}
-
-	if authToken == "" {
-		_ = cmd.Help()
-		return fmt.Errorf("authentication token is empty")
-
-	}
-
-	if stdin == nil {
-		stdin = os.Stdin
-	}
-
-	eventJSON, err := ioutil.ReadAll(stdin)
-	if err != nil {
-		return fmt.Errorf("failed to read stdin: %s", err)
-	}
-
-	event := &types.Event{}
-	err = json.Unmarshal(eventJSON, event)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshal stdin data: %s", err)
-	}
-
-	if err = event.Validate(); err != nil {
-		return fmt.Errorf("failed to validate event: %s", err)
-	}
-
+func checkArgs(event *corev2.Event) error {
 	if !event.HasCheck() {
 		return fmt.Errorf("event does not contain check")
 	}
-
-	return manageIncident(event)
+	return nil
 }
 
-func manageIncident(event *types.Event) error {
+func manageIncident(event *corev2.Event) error {
 	severity := "warning"
 
 	if event.Check.Status < 3 {
@@ -111,7 +72,7 @@ func manageIncident(event *types.Event) error {
 	dedupKey := fmt.Sprintf("%s-%s", event.Entity.Name, event.Check.Name)
 
 	pdEvent := pagerduty.V2Event{
-		RoutingKey: authToken,
+		RoutingKey: config.authToken,
 		Action:     action,
 		Payload:    &pdPayload,
 		DedupKey:   dedupKey,
